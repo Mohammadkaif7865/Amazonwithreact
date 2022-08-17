72e2a694cf99ef736f5291dbef96a210a9c977a8
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-17 21:01:54 +0530

    yes me

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Routing from ' ./Routing';
+import Routing from ' ./routing';
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container)
 root.render(<Routing/>)
--- src/routing.js
+++ src/routing.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Routes, Route} from 'react-router-dom';
 import Header from './header';
+import Home from './Component/home';
 import Footer from './footer';
 export default function Routing(){
     return(
         <Routes>
             <Header/>
+            <Route path="/" component={Home} />
             <Footer/>
         </Routes>
     )
