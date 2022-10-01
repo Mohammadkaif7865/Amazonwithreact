553159799bd406ace937419c6dcbad37299e44b7
mohammad kaif <mohammadkaif051197@gmail.com>
2022-10-01 20:29:24 +0530

    yes me

--- src/routing.js
+++ src/routing.js
@@ -1,15 +1,19 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route,Switch } from "react-router-dom";
 import Footer from "./footer";
 import Home from "./Component/Home";
 import Header from "./header";
+import Ghost from "./Component/Ghost";
 import SearchResult from "./Component/SearchResult";
 
 const Routing = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Switch>
       <Route exact path="/" component={Home} />
       <Route path='/search_result/:category' component={SearchResult} />
+      <Route path="*" component={Ghost} />
+      </Switch>
       <Footer />
     </BrowserRouter>
   );
