4bf23475ce58afdae058b1f64448fb55a432f76d
mohammad kaif <mohammadkaif051197@gmail.com>
2022-10-10 08:56:07 +0530

    ues me

--- src/routing.js
+++ src/routing.js
@@ -1,8 +1,11 @@
-import { BrowserRouter, Route,Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Footer from "./footer";
 import Home from "./Component/Home";
 import Header from "./header";
 import Details from "./Component/Details";
+import Login from "./Component/LoginAuth/login";
+import Register from "./Component/LoginAuth/register";
+import UserInfo from "./Component/LoginAuth/userInfo";
 import Ghost from "./Component/Ghost";
 import SearchResult from "./Component/SearchResult";
 
@@ -11,10 +14,13 @@ const Routing = () => {
     <BrowserRouter>
       <Header />
       <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path='/search_result/:category' component={SearchResult} />
-      <Route path='/details/:id' component={Details} />
-      <Route path="*" component={Ghost} />
+        <Route exact path="/" component={Home} />
+        <Route path='/search_result/:category' component={SearchResult} />
+        <Route path='/login' component={Login}/>
+        <Route path='/userinfo' component={UserInfo}/>
+        <Route path='/register' component={Register}/>
+        <Route path='/details/:id' component={Details} />
+        <Route path="*" component={Ghost} />
       </Switch>
       <Footer />
     </BrowserRouter>
