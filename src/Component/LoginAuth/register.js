8ea929114b0060f6ba18ddd6595e18c4d77f5ef2
mohammad kaif <mohammadkaif051197@gmail.com>
2022-10-14 11:56:44 +0530

    yes me

--- src/Component/LoginAuth/register.js
+++ src/Component/LoginAuth/register.js
@@ -1,73 +1,68 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
 
 const url = "https://authmdkaif.herokuapp.com/api/auth/register";
 
 class Register extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
-        this.state={
-            name:'',
-            email:'',
-            password:'',
-            phone:''
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            phone: ''
         }
     }
 
-    handleChange =(event)=>{
-        this.setState({[event.target.name]:event.target.value})
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
     }
 
     handleSubmit = () => {
-        fetch(url,{
-            method:'POST',
-            headers:{
-                'accept':'application/json',
-                'Content-Type':'application/json'
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-            body:JSON.stringify(this.state)
+            body: JSON.stringify(this.state)
         })
-        .then(this.props.history.push('/login'))
+            .then(this.props.history.push('/login'))
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
-                <div className="container">
-                    <hr/>
-                    <div className="panel panel-primary">
-                        <div className="panel-heading">
-                            <h3>Register</h3>
+                <div className="container" style={{ backgroundColor: "white" }}>
+                    <form>
+                        <legend className='bg bg-primary' style={{ margin: 0, padding: "10px", color: 'white' }}>Register</legend>
+                        <div class="mb-3">
+                            <label htmlFor="email">Email</label>
+                            <input id="email" name="email" className="form-control"
+                                value={this.state.email} onChange={this.handleChange} />
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                         </div>
-                        <div className="panel-body">
-                            <div className="form-group col-md-6">
-                                <label htmlFor="fname">Name</label>
-                                <input id="fname" name="name" className="form-control"
-                                value={this.state.name} onChange={this.handleChange}/>
-                            </div>
-                            <div className="form-group col-md-6">
-                                <label htmlFor="email">Email</label>
-                                <input id="email" name="email" className="form-control"
-                                value={this.state.email} onChange={this.handleChange}/>
-                            </div>
-                            <div className="form-group col-md-6">
-                                <label htmlFor="fname">Password</label>
-                                <input type="password" id="password" name="password" className="form-control"
-                                value={this.state.password} onChange={this.handleChange}/>
-                            </div>
-                            <div className="form-group col-md-6">
-                                <label htmlFor="phone">Phone</label>
-                                <input id="phone" name="phone" className="form-control"
-                                value={this.state.phone} onChange={this.handleChange}/>
-                            </div>
-                            <button className="btn btn-success" onClick={this.handleSubmit}>Register</button>
+                        <div class="mb-3">
+                            <label htmlFor="fname">Name</label>
+                            <input id="fname" name="name" className="form-control"
+                                value={this.state.name} onChange={this.handleChange} />
                         </div>
-                        
-                    </div>
+                        <div class="mb-3">
+                            <label htmlFor="fname">Password</label>
+                            <input type="password" id="password" name="password" className="form-control"
+                                value={this.state.password} onChange={this.handleChange} />
+                        </div>
+                        <div class="mb-3">
+                            <label htmlFor="phone">Phone</label>
+                            <input id="phone" name="phone" className="form-control"
+                                value={this.state.phone} onChange={this.handleChange} />
+                        </div>
+                        <button className="btn btn-success" type='button' onClick={this.handleSubmit}>Register</button>
+                    </form>
                 </div>
             </>
-            
+
         )
     }
 }
