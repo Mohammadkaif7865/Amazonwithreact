5ce063c9ceef8653ee7ba2ab7c8b3c2eda042d4a
mohammad kaif <mohammadkaif051197@gmail.com>
2022-10-19 07:15:49 +0530

    yes me

---
+++ src/Component/Favorities/fav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const cartUrl = 'https://amazoncloneserver.herokuapp.com/userfav';
+function Fav() {
+    return (
+        <>
+            <h1>This is the favorities part</h1>
+        </>
+    )
+}
+export default Fav;
