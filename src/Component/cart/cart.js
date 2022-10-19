fe827aebd7af17acfa907fcfc3c2ae096d982447
mohammad kaif <mohammadkaif051197@gmail.com>
2022-10-19 12:32:30 +0530

    yes me

--- src/Component/cart/cart.js
+++ src/Component/cart/cart.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 const favUrl = 'https://amazoncloneserver.herokuapp.com/cart';
+const favUrlspec = 'https://amazoncloneserver.herokuapp.com/spacific';
 
 function Cart() {
     const [toDisplay, setTodisplay] = useState([]);
     useEffect(() => {
         fetch(`${favUrl}/${sessionStorage.getItem('email')}`, { method: 'GET' }).then((response) => response.json()).then((responseData) => responseData.map((item) => setTodisplay(toDisplay.push(item.itemId))));
     }, []);
+    useEffect(() => {
+        fetch(favUrlspec, { method: 'GET', body: { items: toDisplay } }).then((response) => response.json()).then((responseData) => responseData.map((item) => setTodisplay(toDisplay.push(item.itemId))));
+    }, [toDisplay]);
     console.log(toDisplay);
     return (
         <>
