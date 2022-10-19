51ac38a860a79d50b5f0823acc14f5008b08b848
mohammad kaif <mohammadkaif051197@gmail.com>
2022-10-19 12:03:11 +0530

    yes me

--- src/Component/cart/cart.js
+++ src/Component/cart/cart.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 const favUrl = 'https://amazoncloneserver.herokuapp.com/cart';
 
 function Cart() {
-    const [allFav, setAllFav] = useState("");
+    const [toDisplay, setTodisplay] = useState([]);
     useEffect(() => {
-        fetch(`${favUrl}/${sessionStorage.getItem('email')}`, { method: 'GET' }).then((response) => response.json()).then((responseData) => setAllFav(responseData));
+        fetch(`${favUrl}/${sessionStorage.getItem('email')}`, { method: 'GET' }).then((response) => response.json()).then((responseData) => responseData.map((item) => setTodisplay(toDisplay.push(item.itemId))));
     }, []);
-    console.log(allFav);
+    console.log(toDisplay);
     return (
         <>
             <h1>This is the Cart part</h1>
